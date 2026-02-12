f365a245fd3d2764d728b8ee8b163e63f33206df
James <c4024667@newcastle.ac.uk>
2026-02-12 15:34:01 +0000

    Remove hardcoded data from app\research\page.tsx

---
+++ data/research.ts
@@ -0,0 +1,11 @@
+import Background from "@/assets/background_3.jpg"
+
+export const researchPageContent = {
+  hero: {
+    image: Background,
+    title: "Investment Research",
+  },
+  cta: {
+    viewAnalysis: "View Analysis",
+  },
+}
